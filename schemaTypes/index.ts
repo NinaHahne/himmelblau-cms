2f3c706d0ee05b1b0a06fb17bdb30c3940fff4ce
NinaHahne <ninamariehahne@gmail.com>
2025-01-30 20:32:33 +0100

    feat: update Sanity schema for navigation links
    - Added structured navigation links and footer links to Sanity
    - Improved Sanity Studio readability with a navigation title

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,7 @@
+import navigation from './navigation';
 import openingHours from './openingHours';
 import event from './event';
 import galleryImage from './galleryImage';
 
-export const schemaTypes = [openingHours, event, galleryImage];
+export const schemaTypes = [navigation, openingHours, event, galleryImage];
 
