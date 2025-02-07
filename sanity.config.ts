3e217b0e289d82213c76e9fcc9ccd45bf3a1ff02
NinaHahne <ninamariehahne@gmail.com>
2025-02-07 18:06:56 +0100

    WIP: language support

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemaTypes";
+import { languageFilter } from "@sanity/language-filter";
 
 export default defineConfig({
   name: "default",
@@ -10,7 +11,19 @@ export default defineConfig({
   projectId: "oy7j0u07",
   dataset: "production",
 
-  plugins: [structureTool(), visionTool()],
+  // prettier-ignore
+  plugins: [
+    structureTool(), 
+    visionTool(), 
+    languageFilter({
+      supportedLanguages: [
+          { id: 'de', title: 'Deutsch' },
+          { id: 'en', title: 'English' },
+      ],
+      defaultLanguages: ['de'], // Set the default language(s) here
+      documentTypes: ['navigation', 'openingHours', 'event', 'galleryImage'], // Apply filtering to these types
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
